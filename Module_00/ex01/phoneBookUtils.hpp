919e5c43f8a710c4d88074a5272ef165ded35231
Flamiing <amirlaaouam@gmail.com>
2023-07-20 12:02:03 +0200

    small changes to compile with 42 C++ rules

--- Module_00/ex01/phoneBookUtils.hpp
+++ Module_00/ex01/phoneBookUtils.hpp
@@ -6,12 +6,17 @@
 /*   By: alaaouam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 21:12:07 by alaaouam          #+#    #+#             */
-/*   Updated: 2023/07/20 01:08:42 by alaaouam         ###   ########.fr       */
+/*   Updated: 2023/07/20 12:00:23 by alaaouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOKUTILS_HPP
 # define PHONEBOOKUTILS_HPP
+
+# include "Contact.hpp"
+# include <iostream>
+# include <string>
+# include <iomanip>
 
 std::string	getInput(void);
 void	displayContacts(Contact contacts[]);
