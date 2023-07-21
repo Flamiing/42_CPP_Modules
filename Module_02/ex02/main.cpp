a2e000a2bde55e677abde50734e34a98374b43c3
Flamiing <amirlaaouam@gmail.com>
2023-07-21 17:38:00 +0200

    main

--- Module_02/ex02/main.cpp
+++ Module_02/ex02/main.cpp
@@ -6,16 +6,16 @@
 /*   By: alaaouam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 10:00:03 by alaaouam          #+#    #+#             */
-/*   Updated: 2023/07/21 17:32:30 by alaaouam         ###   ########.fr       */
+/*   Updated: 2023/07/21 17:37:23 by alaaouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void) 
+int main( void ) 
 {
 	Fixed a;
-	Fixed const b( Fixed(5.05f) * Fixed(2) );
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
@@ -23,9 +23,9 @@ int main(void)
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	
-	std::cout << a + a << std::endl;
+	std::cout << b << std::endl;
 	
-	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 	
 	return 0;
 }
